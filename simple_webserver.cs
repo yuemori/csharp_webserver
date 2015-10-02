095ea01cab19da96ed40fcbdb6547c85fa7e1912
yuemori <yuemori@aiming-inc.com>
2015-10-03 03:28:30 +0900

    Add Sqlite class

--- simple_webserver.cs
+++ simple_webserver.cs
@@ -12,38 +12,59 @@ namespace ExampleSimpleWebserver
         static void Main (string[] args)
         {
             List<string> tList = new List<string>();
-            using (var conn = new SqliteConnection("Data Source=dataset.db"))
-            {
-                conn.Open();
-                using (SqliteCommand command = conn.CreateCommand())
-                {
-                    command.CommandText = "SELECT * FROM sqlite_master WHERE type='table'";
-                    using (SqliteDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            tList.Add(reader["tbl_name"].ToString());
-                        }
-                    }
-
-                    if (tList.Count == 0)
-                    {
-                        Console.WriteLine("Table created.");
-                        command.CommandText = "CREATE TABLE matching(id INTEGER PRYMARY KEY, ip_address_1, ip_address_2)";
-                        command.ExecuteNonQuery();
-                    } else {
-                        Console.WriteLine("Table exist.");
-                        foreach (string tableName in tList)
-                        {
-                            Console.WriteLine(tableName);
-                        }
-                    }
-                }
-                conn.Close();
-            }
 
             Console.WriteLine("Running sync server.");
             new SyncServer();
+        }
+    }
+
+    class SqliteHandling
+    {
+        string filename;
+
+        public SqliteHandling(string filename)
+        {
+            this.filename = filename;
+            CreateTableOnlyOnce();
+        }
+
+        public void CreateTableOnlyOnce()
+        {
+            using (var conn = new SqliteConnection("Data Source=dataset.db"))
+            {
+                conn.Open();
+                using (SqliteTransaction transaction = conn.BeginTransaction())
+                {
+                    using (SqliteCommand command = conn.CreateCommand())
+                    {
+                        command.CommandText = "SELECT * FROM sqlite_master WHERE type='table'";
+                        using (SqliteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                tList.Add(reader["tbl_name"].ToString());
+                            }
+                        }
+
+                        if (tList.Count == 0)
+                        {
+                            Console.WriteLine("Table created.");
+
+                            command.CommandText = "CREATE TABLE matching(id INTEGER PRYMARY KEY, ip_address_1, ip_address_2)";
+                            command.ExecuteNonQuery();
+                        } else {
+                            Console.WriteLine("Table exist.");
+
+                            foreach (string tableName in tList)
+                            {
+                                Console.WriteLine(tableName);
+                            }
+                        }
+                    }
+                    transaction.Commit();
+                }
+                conn.Close();
+            }
         }
     }
 
