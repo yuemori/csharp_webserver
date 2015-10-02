11f1c1f9a4f065510259719c58266a3891c613f1
yuemori <yuemori@aiming-inc.com>
2015-10-03 04:39:37 +0900

    Refactor Server class

--- server.cs
+++ server.cs
@@ -8,30 +8,53 @@ namespace Webserver
     {
         public Server()
         {
+            HttpListener listener = GetHttpListener();
+            try
+            {
+                listener.Start();
+            } catch (Exception e)
+            {
+                ErrorHandling(e);
+            }
+            StartListenLoop(listener);
+        }
+
+        HttpListener GetHttpListener()
+        {
             var listener = new HttpListener();
 
             listener.Prefixes.Add("http://localhost:8081/");
             listener.Prefixes.Add("http://127.0.0.1:8081/");
+            return listener;
+        }
 
-            listener.Start();
-
+        void StartListenLoop(HttpListener listener)
+        {
             while (true)
             {
-                try
-                {
-                    var context = listener.GetContext(); //Block until a connection comes in
-                    context.Response.StatusCode = 200;
-                    context.Response.SendChunked = true;
-
-                    var bytes = Encoding.UTF8.GetBytes("waiting");
-                    context.Response.OutputStream.Write(bytes, 0, bytes.Length);
-                    context.Response.Close();
-                }
-                catch (Exception)
-                {
-                    // Client disconnected or some other error - ignored for this example
-                }
+                HttpListenerContext context = listener.GetContext();
+                AddResponseHeader(context);
+                AddResponseOutput(context);
+                context.Response.Close();
             }
+        }
+
+        void AddResponseHeader(HttpListenerContext context)
+        {
+            context.Response.StatusCode = 200;
+            context.Response.SendChunked = true;
+        }
+
+        void AddResponseOutput(HttpListenerContext context)
+        {
+            var bytes = Encoding.UTF8.GetBytes("waiting");
+            context.Response.OutputStream.Write(bytes, 0, bytes.Length);
+        }
+
+        void ErrorHandling(Exception e)
+        {
+            Console.WriteLine("Raise Exception.");
+            Console.WriteLine(e.ToString());
         }
     }
 }
