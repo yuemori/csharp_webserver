1f21d17c8b2519b1a00133a0f1ef05d5d48c705f
yuemori <yuemori@aiming-inc.com>
2015-10-03 03:51:25 +0900

    Use callback for query execute

--- sqlite.cs
+++ sqlite.cs
@@ -8,6 +8,11 @@ namespace Webserver
     public class SqliteHandling
     {
         string filename;
+        readonly string tableDefinition = @"
+            CREATE TABLE matching(id INTEGER PRYMARY KEY, ip_address_1, ip_address_2)
+        ";
+
+        public delegate void CallbackQuery(SqliteCommand command);
 
         public SqliteHandling(string filename)
         {
@@ -16,7 +21,21 @@ namespace Webserver
 
         public void CreateTableOnlyOnce()
         {
-            List<string> tList = new List<string>();
+            ExecuteQuery((command) => {
+                command.CommandText = "SELECT * FROM sqlite_master WHERE type='table'";
+
+                // テーブルがあるかチェックしてなければ作成
+                if (!IsTableExist(command))
+                {
+                    Console.WriteLine("Table created.");
+                    command.CommandText = tableDefinition;
+                    command.ExecuteNonQuery();
+                }
+            });
+        }
+
+        public void ExecuteQuery(CallbackQuery callback)
+        {
             using (var conn = new SqliteConnection("Data Source=" + filename))
             {
                 conn.Open();
@@ -24,35 +43,26 @@ namespace Webserver
                 {
                     using (SqliteCommand command = conn.CreateCommand())
                     {
-                        command.CommandText = "SELECT * FROM sqlite_master WHERE type='table'";
-                        using (SqliteDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                tList.Add(reader["tbl_name"].ToString());
-                            }
-                        }
-
-                        if (tList.Count == 0)
-                        {
-                            Console.WriteLine("Table created.");
-
-                            command.CommandText = "CREATE TABLE matching(id INTEGER PRYMARY KEY, ip_address_1, ip_address_2)";
-                            command.ExecuteNonQuery();
-                        } else {
-                            Console.WriteLine("Table exist.");
-
-                            foreach (string tableName in tList)
-                            {
-                                Console.WriteLine(tableName);
-                            }
-                        }
+                        callback(command);
                     }
                     transaction.Commit();
                 }
                 conn.Close();
             }
         }
-    }
 
+        private bool IsTableExist(SqliteCommand command)
+        {
+            List<string> tList = new List<string>();
+            using (SqliteDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    tList.Add(reader["tbl_name"].ToString());
+                }
+            }
+
+            return tList.Count != 0;
+        }
+    }
 }
